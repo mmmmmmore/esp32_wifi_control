3f0851af2e620fe7264a2d06b37fce8fc2c262f0
mmmmmmore <maochuneric@gmail.com>
2025-09-22 13:49:39 +0800

    Update wifi_ap.c

--- protocol/wifi_ap/wifi_ap.c
+++ protocol/wifi_ap/wifi_ap.c
@@ -4,6 +4,10 @@ static const char *TAG = "wifi_ap";
 
 void wifi_init_softap(void)
 {
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_ap();
+
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
     ESP_ERROR_CHECK(esp_wifi_init(&cfg));
     ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
@@ -23,3 +27,4 @@ void wifi_init_softap(void)
 
     ESP_LOGI(TAG, "WiFi SoftAP started. SSID: ESP32-SoftAP");
 }
+
