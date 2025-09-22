12f7ca41a1861e5a2c3600869858fe50aa705bc2
mmmmmmore <maochuneric@gmail.com>
2025-09-22 19:28:05 +0800

    Update wifi_ap.c

--- protocol/wifi_ap/wifi_ap.c
+++ protocol/wifi_ap/wifi_ap.c
@@ -1,29 +1,38 @@
 #include "wifi_ap.h"
 
+
+#define WIFI_SSID "ESP32-AP"
+#define WIFI_PASS "12345678"
+#define LED_GPIO GPIO_NUM_2
+
+static const char *TAG = "main";
+
+// 从 sdkconfig 中读取 SSID 和密码
+const char* ssid = CONFIG_WIFI_SOFTAP_SSID;
+const char* password = CONFIG_WIFI_SOFTAP_PASSWORD;
+
 static const char *TAG = "wifi_ap";
 
-void wifi_init_softap(void)
-{
-//    ESP_ERROR_CHECK(esp_netif_init());
-//    ESP_ERROR_CHECK(esp_event_loop_create_default());
-//   esp_netif_create_default_wifi_ap();
-
+void wifi_init_softap(void) {
+    esp_netif_create_default_wifi_ap();
     wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    esp_wifi_init(&cfg);
 
-    wifi_config_t ap_config = {
+    wifi_config_t wifi_config = {
         .ap = {
-            .ssid = "ESP32-SoftAP",
-            .ssid_len = strlen("ESP32-SoftAP"),
-            .password = "niwenwoa",
+            .ssid = "",
+            .ssid_len = strlen(ssid),
             .max_connection = 4,
             .authmode = WIFI_AUTH_WPA_WPA2_PSK
         },
     };
 
-    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &ap_config));
-    ESP_ERROR_CHECK(esp_wifi_start());
+    strncpy((char*)wifi_config.ap.ssid, ssid, sizeof(wifi_config.ap.ssid));
+    strncpy((char*)wifi_config.ap.password, password, sizeof(wifi_config.ap.password));
+
+    esp_wifi_set_mode(WIFI_MODE_AP);
+    esp_wifi_set_config(WIFI_IF_AP, &wifi_config);
+    esp_wifi_start();
 
     ESP_LOGI(TAG, "WiFi SoftAP started. SSID: ESP32-SoftAP");
 }
