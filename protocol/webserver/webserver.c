b3df79b716aae58503e8105bcbd8d288be57a111
mmmmmmore <maochuneric@gmail.com>
2025-09-23 13:00:07 +0800

    add the head

--- protocol/webserver/webserver.c
+++ protocol/webserver/webserver.c
@@ -1,6 +1,7 @@
 #include "esp_http_server.h"
 #include "stream_control.h"
 #include "ov7670_fifo.h"
+#include "esp_log.h"
 
 static esp_err_t toggle_handler(httpd_req_t *req) {
     char buf[8] ={0};
