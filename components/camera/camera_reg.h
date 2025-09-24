8d5586cb47ff82311635fc73af06d41d2c4985d2
mmmmmmore <maochuneric@gmail.com>
2025-09-24 15:05:40 +0800

    Create camera_reg.h

---
+++ components/camera/camera_reg.h
@@ -0,0 +1,23 @@
+#ifndef CAMERA_REGS_H
+#define CAMERA_REGS_H
+
+#define REG_COM7        0x12
+#define REG_RGB444      0x8c
+#define REG_COM15       0x40
+#define REG_COM1        0x04
+#define REG_COM9        0x14
+#define REG_COM3        0x0C
+#define REG_COM14       0x3E
+#define REG_SCALING_XSC 0x70
+#define REG_SCALING_YSC 0x71
+#define REG_SCALING_DSP 0x72
+#define REG_HSTART      0x17
+#define REG_HSTOP       0x18
+#define REG_HREF        0x32
+#define REG_VSTART      0x19
+#define REG_VSTOP       0x1A
+#define REG_VREF        0x03
+#define REG_CLKRC       0x11
+#define REG_COM11       0x3B
+
+#endif // CAMERA_REGS_H
