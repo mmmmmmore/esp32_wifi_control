b5dc71f22b3eb165605794c5a3b7ba54d957321e
mmmmmmore <maochuneric@gmail.com>
2025-09-24 16:25:07 +0800

    Update camera.c

--- components/camera/camera.c
+++ components/camera/camera.c
@@ -10,19 +10,19 @@
 static const char *TAG = "camera";
 
 const struct regval_list ov7670_qvga_rgb565[] = {
-    { REG_COM7, 0x80 },
-    { REG_CLKRC, 0x80 },
-    { REG_COM11, 0x0A },
-    { REG_COM7, 0x04 },
-    { REG_RGB444, 0x00 },
-    { REG_COM15, 0xD0 },
-    { REG_COM1, 0x00 },
-    { REG_COM9, 0x6A },
-    { REG_COM3, 0x04 },
-    { REG_COM14, 0x19 },
-    { REG_SCALING_XSC, 0x3A },
-    { REG_SCALING_YSC, 0x35 },
-    { REG_SCALING_DSP, 0x03 },
+    { REG_COM7, 0x80 },     //reverse
+    { REG_CLKRC, 0x80 },    //use external clk signal
+    { REG_COM11, 0x0A },    //use BD50ST and expourse time smaller
+    { REG_COM7, 0x04 },     //output RGB mode
+    { REG_RGB444, 0x00 },    //RGB44 disable
+    { REG_COM15, 0xD0 },    //output 00-FF, RGB565 match with RGB444 low
+    { REG_COM1, 0x00 },    ///disable general control 
+    { REG_COM9, 0x6A },     //128X gain, 
+    { REG_COM3, 0x04 },    //enable com3 
+    { REG_COM14, 0x19 },    // SCW maunal adjust, /2
+    { REG_SCALING_XSC, 0x3A },    //default value
+    { REG_SCALING_YSC, 0x35 },    //default value
+    { REG_SCALING_DSP, 0x03 },    //
     { REG_HSTART, 0x16 },
     { REG_HSTOP, 0x04 },
     { REG_HREF, 0x24 },
