7f0cd793237bca9ca0707cb61c942507958c9b5b
mmmmmmore <maochuneric@gmail.com>
2025-09-24 18:36:34 +0800

    re-structure the code

--- protocol/sccb/sccb.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-#include <stdbool.h>
-
-bool sccb_init(void);
-bool sccb_write(uint8_t reg_addr, uint8_t data);
-bool sccb_read(uint8_t reg_addr, uint8_t *data);
-
