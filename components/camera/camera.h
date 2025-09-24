d8133dd5485da66c39c79e940cf05af0290324c8
mmmmmmore <maochuneric@gmail.com>
2025-09-24 15:04:15 +0800

    Update camera.h

--- components/camera/camera.h
+++ components/camera/camera.h
@@ -1,6 +1,23 @@
 #pragma once
-
 #include <stdbool.h>
+
+#ifndef CAMERA_H
+#define CAMERA_H
+
+// Structure for register-value pair
+struct regval_list {
+    unsigned char reg;
+    unsigned char val;
+};
+
+// OV7670 initialization function for QVGA + RGB565
+void ov7670_init_qvga_rgb565();
+
+
+
 
 // Initialize the OV7670 camera sensor
 bool camera_init(void);
+
+
+#endif // CAMERA_H
