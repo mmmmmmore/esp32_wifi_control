5882684b23e66250a23e4203324fbc2f59b31ca1
mmmmmmore <maochuneric@gmail.com>
2025-09-24 10:13:07 +0800

    Update global_gpio.h

---
+++ components/include/global_gpio.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// FIFO control pins
+#define PIN_WR     GPIO_NUM_2
+#define PIN_RRST   GPIO_NUM_3
+#define PIN_OE     GPIO_NUM_4
+#define PIN_RCLK   GPIO_NUM_5
+
+// FIFO data pins
+#define PIN_D0     GPIO_NUM_6
+#define PIN_D1     GPIO_NUM_7
+#define PIN_D2     GPIO_NUM_8
+#define PIN_D3     GPIO_NUM_9
+#define PIN_D4     GPIO_NUM_10
+#define PIN_D5     GPIO_NUM_11
+#define PIN_D6     GPIO_NUM_12
+#define PIN_D7     GPIO_NUM_13
+
+// VSYNC pin from OV7670
+#define PIN_VSYNC  GPIO_NUM_14
