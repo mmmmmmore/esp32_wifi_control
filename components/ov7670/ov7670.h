c3f7c61a1fe3ad05a6ba3441f52994ff6c3b1ce0
mmmmmmore <maochuneric@gmail.com>
2025-09-24 13:56:22 +0800

    Update ov7670.h

--- components/ov7670/ov7670.h
+++ components/ov7670/ov7670.h
@@ -1,13 +1,6 @@
-#ifndef OV7670_H
-#define OV7670_H
+#pragma once
 
-#include <stdint.h>
 #include <stdbool.h>
 
-// 初始化摄像头
-void ov7670_init(void);
-
-// 可选：设置窗口大小（如使用 AL422B FIFO）
-void ov7670_window_set(uint16_t startX, uint16_t startY, uint16_t width, uint16_t height);
-
-#endif // OV7670_H
+// 初始化 OV7670 摄像头寄存器
+bool ov7670_config(void);
