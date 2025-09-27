f27f567672ddc3a2bf5a6a0bd73272f20204e928
mmmmmmore <maochuneric@gmail.com>
2025-09-27 13:45:15 +0800

    Update ov7670_handler.h

--- handler/ov7670_handler/ov7670_handler.h
+++ handler/ov7670_handler/ov7670_handler.h
@@ -15,6 +15,10 @@ void fifo_reset_write_pointer(void);
 // 初始化采集任务（可选）
 void ov7670_handler_init(void);
 
+void ov7670_read_frame(uint8_t *buffer, size_t size);
+
+
 // 采集一帧图像，返回图像缓冲区指针和大小
 uint8_t* ov7670_capture_frame(const image_size_t* size, size_t* out_len);
+
 
