7320a80859178c864c5b89dc79e392c607367ba5
mmmmmmore <maochuneric@gmail.com>
2025-09-27 13:35:26 +0800

    udate

--- main/main.c
+++ main/main.c
@@ -11,9 +11,26 @@
 #include "jpeg.h"
 #include "ov7670_handler.h"
 #include "sccb.h"
-#include "spiffs.h"
+//#include "spiffs.h"
 
 
+
+
+void init_spiffs(){
+    esp_vfs_spiffs_conf_t conf ={
+        .base_path = "/spiffs",
+        .partition_label = "spiffs",
+        .max_files =5,
+        .format_if_mount_failed = true
+    } ;
+    
+    esp_err_t ret = esp_vfs_spiffs_register(&conf);
+    if (ret != ESP_OK){
+        ESP_LOGE("SPIFFS:","Failed to mount or format filesystem : %s", esp_err_to_name(ret));
+    }else{
+        ESP_LOGI("SPIFFS:","SPIFFS mounted successfully");
+    }
+}
 
 void app_main(void) {
     esp_err_t ret = nvs_flash_init();
