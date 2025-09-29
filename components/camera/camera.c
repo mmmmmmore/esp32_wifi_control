56053b2321cfc37c0e8eefebd8a4cf7d6029a5a8
mmmmmmore <maochuneric@gmail.com>
2025-09-29 15:27:23 +0800

    Update camera.c

--- components/camera/camera.c
+++ components/camera/camera.c
@@ -8,8 +8,6 @@
 #include "driver/i2c.h"
 #include "i2c_config.h"
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
 #include "driver/gpio.h"
 // static const char *TAG = "camera";
 
@@ -45,8 +43,8 @@ void vsync_debug_task(void *arg) {
 void i2c_master_init() {
     i2c_config_t conf = {
         .mode = I2C_MODE_MASTER,
-        .sda_io_num = I2C_MASTER_SDA_IO,
-        .scl_io_num = I2C_MASTER_SCL_IO,
+        .sda_io_num = PIN_SDA,
+        .scl_io_num = PIN_SCL,
         .sda_pullup_en = GPIO_PULLUP_ENABLE,
         .scl_pullup_en = GPIO_PULLUP_ENABLE,
         .master.clk_speed = I2C_MASTER_FREQ_HZ,
