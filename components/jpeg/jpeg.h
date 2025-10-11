1f8e5c289a0effb1f37f255353ef38de9068475c
mmmmmmore <maochuneric@gmail.com>
2025-10-11 10:46:24 +0800

    Update jpeg.h

--- components/jpeg/jpeg.h
+++ components/jpeg/jpeg.h
@@ -10,6 +10,11 @@
 // JPEG 编码函数接口
 // 输入：原始图像数据（RGB565）、图像尺寸
 // 输出：JPEG 数据指针和长度（动态分配，需外部释放）
-uint8_t* jpeg_encode_rgb565(const uint8_t* rgb_data, size_t rgb_len, uint16_t width, uint16_t height, size_t* jpeg_len);
+uint8_t* jpeg_encode_rgb565(
+    const uint8_t* rgb_data, 
+    size_t rgb565_len, 
+    uint16_t width, 
+    uint16_t height, 
+    size_t* jpeg_len);
 
 #endif
