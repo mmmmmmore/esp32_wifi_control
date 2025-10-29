0c968d3503b3a9d8e311340b311260ef9eb59782
mmmmmmore <maochuneric@gmail.com>
2025-10-29 17:55:19 +0800

    Update ota_handler.h

--- handler/ota_handler/ota_handler.h
+++ handler/ota_handler/ota_handler.h
@@ -1,8 +1,19 @@
-#pragma once
+#ifndef OTA_HANDLER_H
+#define OTA_HANDLER_H
 
-#include <stdint.h>
-#include <stddef.h>
-#include "esp_err.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-// Interface to capture and encode a JPEG frame from OV7670
-esp_err_t ov7670_handler_get_jpeg(uint8_t **jpeg_data, size_t *jpeg_size);
+/**
+ * @brief Start OTA update from the given URL.
+ *
+ * @param ota_url The HTTP or HTTPS URL pointing to the firmware binary.
+ */
+void ota_start(const char *ota_url);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // OTA_HANDLER_H
